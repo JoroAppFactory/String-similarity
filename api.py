b42188a35edac09f951355c2b610e1dab1ba349a
naskomsm <naskokolev00@gmail.com>
2021-11-09 16:47:05 +0200

    test

--- api.py
+++ api.py
@@ -22,10 +22,10 @@ app = Flask(__name__)
 api = Api(app, version='1.0', title='Bevvy OCR API',
           description='Bevvy OCR API docs')
 
-api.add_resource(ClosestMatch, '/match/')
-api.add_resource(ClosestMatchFromFile, '/matching_from_file/')
-api.add_resource(Autocorrect, '/autocorrect/')
-api.add_resource(Similarity, '/similarity/')
+api.add_resource(ClosestMatch, '/match')
+api.add_resource(ClosestMatchFromFile, '/matching_from_file')
+api.add_resource(Autocorrect, '/autocorrect')
+api.add_resource(Similarity, '/similarity')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
