43b99e6dfdf2f0c274a74cb0aea998575e021d0f
naskomsm <naskokolev00@gmail.com>
2021-11-09 15:30:00 +0200

    test

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 # General
+import json
 from flask import Flask
 from flask_restx import Api
 
@@ -6,16 +7,26 @@ from flask_restx import Api
 from resources.closest_match import ClosestMatchFromFile, ClosestMatch
 from resources.auto_correct import Autocorrect
 from resources.similarity import Similarity
+from resources.services.similarity_service import Result
 
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Bevvy OCR API',
           description='Bevvy OCR API docs')
 
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Result):
+            return {"value": obj.value, "percentage": obj.percentage}
+        return json.JSONEncoder.default(self, obj)
+
+
+app.json_encoder = CustomEncoder
+
 api.add_resource(ClosestMatch, '/match/')
 api.add_resource(ClosestMatchFromFile, '/matching_from_file/')
 api.add_resource(Autocorrect, '/autocorrect/')
 api.add_resource(Similarity, '/similarity/')
- 
 
 
 if __name__ == '__main__':
